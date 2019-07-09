998bc36785bc9b548ab6b1b6264d61463a57650b
meti <49797027+metig@users.noreply.github.com>
2019-07-08 20:36:01 -0700

    style changes

--- tv-show-app/src/app/landing/landing.component.ts
+++ tv-show-app/src/app/landing/landing.component.ts
@@ -13,6 +13,7 @@ export class LandingComponent implements OnInit {
       name1: "Game of Thrones",
       officialSite1: "http://www.hbo.com/game-of-thrones",
       image1: "http://static.tvmaze.com/uploads/images/medium_portrait/190/476117.jpg",
+      
       name2: "Stranger things",
       officialSite2: "https://www.netflix.com/title/80057281",
       image2: "http://static.tvmaze.com/uploads/images/medium_portrait/200/501942.jpg",
